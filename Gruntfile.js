9e0846314af1db04019ee40b92b760c220c58e22
Natalia <natalie.miku@gmail.com>
2014-09-13 16:42:15 +1200

    Change Gruntfile.js \n \nFix for grant-bower task so grunt catches all bootstrap dependences, Remove bower.json overrides property, Add grunt-contrib-clean to remove unused bootstrap scss files, Add custom bootstrap.scss to handel installed bootstrap dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,19 +2,6 @@ module.exports = function(grunt) {
 	var skinDir = 'theme/skin/frontend/yourcar-theme/default/';
 	var appDir = 'theme/app/design/frontend/yourcar-theme/default/';
 
-	/*var jsLibs = [
-			'bower_components/jquery/dist/jquery.min.js',
-			'bower_components/html5shiv/dist/html5shiv.min.js',
-			'bower_components/respond/dest/respond.min.js'
-	];
-	var jsBootstrap = [
-			'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap.js'
-	];
-	var sassBootstrap =[
-			'bower_components/bootstrap-sass-official/assets/stylesheets/bootstrap.scss',
-			'bower_components/bootstrap-sass-official/assets/stylesheets/bootstrap-compass.scss'
-	];*/
-
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 		compass: {
@@ -31,57 +18,86 @@ module.exports = function(grunt) {
 			options: {
 				livereload: true
 			},
+			grunt: { 
+				files: ['Gruntfile.js'] 
+			},
 			livereload: {
 				files: [
-					skinDir + 'src/scss/{,*/}*.scss'
+					appDir + '**/*.xml',
+					skinDir + 'src/scss/{,*/}*.scss',
+					[
+						skinDir + 'src/*.js', 
+						skinDir + 'src/**/*.js',
+						'!' + skinDir + 'js/*.min.js'
+					]
 				],
-				tasks: ['compass']
+				tasks: [
+					'compass',
+					'jshint',
+					'uglify'
+				]
 			}
+		},
+		jshint: {
+			options: {
+		        curly: true,
+		        eqeqeq: true,
+		        eqnull: true,
+		        browser: true,
+		        globals: {
+		          jQuery: true
+		        }
+		    },
+			all: [
+				'Gruntfile.js',
+				skinDir + 'src/js/{,*/}*.js'
+			]
 		},
 		uglify: {
 			dist: {
 				options: {
 					//mangle: false - preserve variable and function names
 					//so they are not clash with Magento JS
-					mangle: false
+					mangle: false,
+					compress: true
 				},
-				files: {
-					//'theme/skin/frontend/yourcar-theme/default/js/jquery.scripts.min.js': [skinDir + 'src/js/scripts.js'], 
-					//'theme/skin/frontend/yourcar-theme/default/js/bootstrap.min.js': [skinDir + 'src/js/bootstrap.js'],
-
-				}
+				my_target: {
+			      files: [{
+			          expand: true,
+			          ext: '.min.js',
+			          cwd: skinDir + 'src/js',
+			          src: '**/*.js',
+			          dest: skinDir + 'js'
+			      }]
+			    }
+				/*files: {
+					skinDir + 'js/html5shiv.min.js': [skinDir + 'src/js/html5shiv.js'], 
+					skinDir + 'js/jquery.min.js': [skinDir + 'src/js/jquery.js'], 
+					skinDir + 'js/respond.min.js': [skinDir + 'src/js/respond.js'],
+					skinDir + 'js/respond.min.js': [skinDir + 'src/js/bootstrap/bootstrap.js'],
+					skinDir + 'js/jquery.scripts.min.js': [skinDir + 'src/js/vendor/jquery.scripts.js']
+				}*/
 			}
 		},
-		/*bower: {
-		  install: {
-		  	options: {
-		  		targetDir: skinDir + 'src',
-		  		layout: 'byType',
-		  		cleanTargetDir: false,
-		  		install: true		  		
-		  	}
-		  }
-		}*/
 		bower: {
 		  dev: {
 		  	dest: skinDir + 'src',
-		  	fonts_dest: skinDir + 'src/fonts',
-		    js_dest: skinDir + 'src/js',		    
+		  	js_dest: skinDir + 'src/js',		    
+		  	scss_dest: skinDir + 'src/scss',		    
 		    options: {
+		      stripAffix: true,	
 		      packageSpecific: {
 		        "bootstrap-sass-official": {		          
 		          js_dest: skinDir + 'src/js/bootstrap',
-      			  scss_dest: skinDir + 'src/scss/bootsrap',
+      			  scss_dest: skinDir + 'src/scss/modules/bootstrap',
       			  dest: skinDir + 'src/fonts/bootsrap',
       			  keepExpandedHierarchy: true,
 		          files: [
 		           	'assets/javascripts/**',
 		           	'assets/fonts/**',
 		           	'assets/stylesheets/**'
-		          ]
-			             			  
-		        }
-		        
+		          ]		             			  
+		        }	        
 		      }
 		    }
 		  }
@@ -90,23 +106,33 @@ module.exports = function(grunt) {
 			install: {
 
 			}
+		},
+		clean: {
+		  scss: [
+		  		skinDir + 'src/scss/modules/bootstrap/_bootstrap.scss',
+		  		skinDir + 'src/scss/modules/bootstrap/_bootstrap-*.scss'
+		  ]
 		}
 
 		
 	});
 	grunt.loadNpmTasks('grunt-contrib-compass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-bower-task');
 	grunt.renameTask("bower", "bowerInstall");
 	grunt.loadNpmTasks('grunt-bower');
 	grunt.registerTask('default', [
 			'compass',
-			'uglify',
+			'jshint',
+			'uglify'
 	]);
 	grunt.registerTask('build', [
 			'bowerInstall',
-			'bower'
+			'bower',
+			'clean'
 	]);
 
 	
