47a44e60aa377855c07f47482a84e59b16752360
Natalia <natalie.miku@gmail.com>
2014-09-19 00:00:35 +1200

    Header design
    Lack of the javascript to slide out/in header panel

---
+++ config.rb
@@ -0,0 +1,38 @@
+# Require any additional compass plugins here.
+#add_import_path = "bower_components"
+#require "animate"
+
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+#project_path = "theme/skin/frontend/yourcar-theme/default/"
+#css_dir = "src/css"
+#sass_dir = "src/scss"
+#images_dir = "src/images"
+#javascripts_dir = "src/js"
+#fonts_dir = "fonts"
+fonts_dir = "/skin/frontend/yourcar-theme/default/fonts"
+##sourcemap = true
+
+
+
+# You can select your preferred output style here (can be overridden via the command line):
+#output_style = :expanded or :nested or :compact or :compressed
+#output_style = :compressed
+#output_style = :expanded
+output_style = :nested
+
+environment = :development
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+#line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
